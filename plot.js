3be9b737b8b83fcd05c744d8c579c87dbd7b78a1
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-03-14 17:50:02 -0500

    retrieved the 7 fastest growing cities and plotted with plotly

--- plot.js
+++ plot.js
@@ -1,1 +1,23 @@
 var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse();
+
+var topSevenCities = sortedCities.slice(0,7)
+
+var topSevenCityNames = topSevenCities.map(city => city.City);
+
+var topSevenCityGrowths = topSevenCities.map(city => parseInt(city.Increase_from_2016));
+
+var trace = {
+    x: topSevenCityNames,
+    y: topSevenCityGrowths,
+    type: 'bar'
+}
+
+var data = [trace]
+
+var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: {title: 'City'},
+    yaxis: {title: 'Population Growth, 2016-2017'}
+}
+
+Plotly.newPlot('bar-plot', data, layout)
