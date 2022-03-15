72df308e51c470346bf72d5d3d8f86e0fcd709ab
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-03-15 14:38:35 -0500

    created functions for differnt plots

--- dashboard.js
+++ dashboard.js
@@ -50,3 +50,103 @@ function buildMetadata(sample) {
         });
     });
 }
+
+// Creating a buildCharts function
+function buildCharts(sample) {
+    // Using d3.json to load and retrieve the samples.json file
+    d3.json('samples.json').then((data) => {
+        // Creating a variable that holds the samples array
+        var samples = data.samples;
+        // Creating a variable that filters the samples for the object with the desired sample number
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        // Creating a variable that holds the first sample in the array
+        var result = resultArray[0];
+
+        // Creating variables thathold the otu_ids, otu_labels, and sample_values
+        var ids = result.otu_ids;
+        var labels = result.otu_labels.slice(0,10).reverse();
+        var values = result.sample_values.slice(0,10).reverse();
+        var bubbleLabels = result.otu_labels;
+        var bubbleValues = result.sample_values;
+        
+        // Creating the yticks for the bar chart
+        // by getting the top 10 otu_ids and map them in a descending order
+        // This makes the otu_ids with the least amount of bacteria plotted last
+        var yticks = ids.map(sampleObj => 'OTU ' + sampleObj).slice(0,10).reverse();
+
+        console.log(yticks)
+
+        // Creating the trace for the bar chart
+        var barData = [{
+            x: values,
+            y: yticks,
+            type: 'bar',
+            orientation: 'h',
+            text:labels
+        }];
+
+        // Creating the layout for the bar chart
+        var barLayout = {
+            title: 'Top 10 Bacteria Cultures Found'
+        };
+
+        // Using Plotly to plot the data with the layout
+        Plotly.newPlot('bar', barData, barLayout);
+
+        // Create a bubble chart
+        var bubbleData = [{
+            x: ids,
+            y: bubbleValues,
+            text: bubbleLabels,
+            mode: 'markers',
+            marker: {
+                size: bubbleValues,
+                color: bubbleValues,
+                colorscale: "Portland"
+            }
+        }];
+
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            xaxis: {title: 'OTU ID'},
+            yaxis: {title: 'Sample Volume'},
+            automargin: true,
+            hovermode: 'closest'
+        };
+
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout)
+
+        // Creating a Gauge Chart
+        var metadata = data.metadata;
+        var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);
+
+        var gaugeResult = gaugeArray[0];
+
+        var wfreqs = gaugeResult.wfreq;
+
+        var gaugeData = [{
+            value: wfreqs,
+            type: "indicator",
+            mode: "gauge+number",
+            title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
+            gauge: {
+                axis: {range: [null, 10], dtick:'2'},
+                bar: {color:"black"},
+                steps:[
+                    {range: [0, 2], color: "red"},
+                    {range: [2, 4], color: "orange"},
+                    {range: [4, 6], color: "yellow"},
+                    {range: [6, 8], color: "lightgreen"},
+                    {range: [8, 10], color: "green"}
+                ],
+                dtick: 2
+            }
+        }];
+
+        var gaugeLayout = {
+            automargin: true
+        };
+
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout)
+    });
+}
